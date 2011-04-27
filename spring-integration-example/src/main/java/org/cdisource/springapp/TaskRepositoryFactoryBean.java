1e64440d6ccb57268b75c51c925d768d69e8bb08
CDI Advocate <cloud.potato.salad@gmail.com>
2011-04-27 16:37:55 -0700

    added update factory bean

--- spring-integration-example/src/main/java/org/cdisource/springapp/TaskRepositoryFactoryBean.java
+++ spring-integration-example/src/main/java/org/cdisource/springapp/TaskRepositoryFactoryBean.java
@@ -1,41 +1,28 @@
 package org.cdisource.springapp;
 
 import javax.enterprise.inject.spi.BeanManager;
-import javax.naming.InitialContext;
 
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.cdisource.beancontainer.BeanContainer;
 import org.cdisource.beancontainer.BeanContainerImpl;
-import org.cdisource.beancontainer.BeanContainerInitializationException;
 
-public class TaskRepositoryFactoryBean implements FactoryBean<TaskRepository> {
+public class TaskRepositoryFactoryBean implements FactoryBean<TaskRepository>, InitializingBean {
 
-	private BeanContainer beanContainer = null;
-	private final String BEAN_MANAGER_LOCATION = "java:comp/BeanManager";
-
+	private BeanContainer beanContainer;
+	private BeanManager beanManager;
 	
+	public void setBeanManager(BeanManager beanManager) {
+		this.beanManager = beanManager;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		beanContainer = new BeanContainerImpl(beanManager);
+	}
+
 	@Override
 	public TaskRepository getObject() throws Exception {
-
-		if (beanContainer==null) {
-			InitialContext ic = new InitialContext();
-			Object bean = null;
-			try {
-				bean = ic.lookup(BEAN_MANAGER_LOCATION);
-			} catch (Exception e) {
-				throw new BeanContainerInitializationException("Unable to lookup BeanManager instance in JNDI", e);
-			}
-			if (bean == null) {
-				throw new BeanContainerInitializationException(
-						"Null value returned when looking up the BeanManager from JNDI");
-			}
-			if (bean instanceof BeanManager) {
-				beanContainer = new BeanContainerImpl((BeanManager)bean);
-			} else {
-				String msg = "Looked up JNDI Bean is not a BeanManager instance, bean type is " + bean.getClass().getName();
-				throw new BeanContainerInitializationException(msg);
-			}
-		}
 		return beanContainer.getBeanByType(TaskRepository.class);
 	}
 
@@ -48,5 +35,6 @@ public class TaskRepositoryFactoryBean implements FactoryBean<TaskRepository> {
 	public boolean isSingleton() {
 		return true;
 	}
+
 
 }
